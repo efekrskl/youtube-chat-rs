1f8e6f0d6fe1b3fbe4d72901b5ff9c2ae0e0712c
Efe Karasakal <dogukankrskl@gmail.com>
2026-02-25 19:41:39 +0100

    feat: log individual types

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,13 @@
 use crate::http_types::{SearchResponse, VideoListResponse};
+use crate::youtube_api_v3;
+use crate::youtube_api_v3::LiveChatMessageListRequest;
+use crate::youtube_api_v3::v3_data_live_chat_message_service_client::V3DataLiveChatMessageServiceClient;
 use anyhow::{Context, bail};
 use reqwest::Url;
 use reqwest::header::{AUTHORIZATION, HeaderValue};
+use tonic::Request;
+use tonic::metadata::MetadataValue;
+use tonic::transport::{Channel, ClientTlsConfig};
 
 pub struct AppClient {
     token: String,
@@ -113,14 +119,81 @@ impl AppClient {
         let Some(channel_id) = self.channel_id_by_name(channel_name).await? else {
             return Ok(None);
         };
-        println!("{channel_id}");
 
         let Some(live_stream_id) = self.live_video_id_by_channel_id(&channel_id).await? else {
             return Ok(None);
         };
 
-        println!("{live_stream_id}");
-
         self.find_chat_id_by_live_video_id(&live_stream_id).await
+    }
+}
+
+impl AppClient {
+    pub async fn listen(&self, live_chat_id: &str) -> anyhow::Result<()> {
+        let tls = ClientTlsConfig::new().with_native_roots();
+        let channel: Channel = Channel::from_static("https://youtube.googleapis.com")
+            .tls_config(tls)?
+            .connect()
+            .await?;
+
+        let mut client = V3DataLiveChatMessageServiceClient::new(channel);
+
+        let mut next_page_token: Option<String> = None;
+
+        loop {
+            let req = LiveChatMessageListRequest {
+                part: vec![
+                    "id".to_string(),
+                    "snippet".to_string(),
+                    "authorDetails".to_string(),
+                ],
+                live_chat_id: Some(live_chat_id.to_string()),
+                max_results: Some(20),
+                page_token: next_page_token.clone(),
+                profile_image_size: Some(0),
+                hl: Some("en".to_string()),
+            };
+
+            let mut request = Request::new(req);
+            let auth: MetadataValue<_> = format!("Bearer {}", self.token).parse()?;
+            request.metadata_mut().insert("authorization", auth);
+
+            let mut stream = client.stream_list(request).await?.into_inner();
+
+            while let Some(resp) = stream.message().await? {
+                for item in resp.items.iter() {
+                    use crate::youtube_api_v3::live_chat_message_snippet::type_wrapper::Type as MessageType;
+
+                    let Some(snippet) = item.snippet.as_ref() else {
+                        continue;
+                    };
+
+                    match snippet.r#type() {
+                        MessageType::TextMessageEvent => {
+                            let msg = snippet.display_message.as_ref().unwrap();
+                            let auth = item
+                                .author_details
+                                .as_ref()
+                                .unwrap()
+                                .display_name
+                                .as_ref()
+                                .unwrap();
+
+                            println!("{auth}: {msg}");
+                        }
+                        MessageType::NewSponsorEvent => { /* new member */ }
+                        _ => {}
+                    }
+                }
+
+                next_page_token = resp.next_page_token.clone();
+            }
+
+            if next_page_token.is_none() {
+                break;
+            }
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,14 @@ mod http_types;
 
 use crate::auth::auth;
 use crate::client::AppClient;
-use crate::youtube_api_v3::LiveChatMessageListRequest;
-use crate::youtube_api_v3::v3_data_live_chat_message_service_client::V3DataLiveChatMessageServiceClient;
-use anyhow::Result;
 use dialoguer::Input;
-use tonic::Request;
-use tonic::metadata::MetadataValue;
-use tonic::transport::Channel;
-use tonic::transport::ClientTlsConfig;
 
 pub mod youtube_api_v3 {
     tonic::include_proto!("youtube.api.v3");
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> anyhow::Result<()> {
     let token = auth().await?;
     let app_client = AppClient::new(&token)?;
     let channel_name: String = Input::new()
@@ -27,42 +20,12 @@ async fn main() -> Result<()> {
         .interact_text()?;
     let live_chat_id = app_client.find_chat_by_channel_name(&channel_name).await?;
 
-    let tls = ClientTlsConfig::new().with_native_roots();
-    let channel: Channel = Channel::from_static("https://youtube.googleapis.com")
-        .tls_config(tls)?
-        .connect()
-        .await?;
+    match live_chat_id {
+        Some(id) => app_client.listen(&id).await,
+        None => {
+            eprintln!("Couldn't find live chat id.");
 
-    let mut client = V3DataLiveChatMessageServiceClient::new(channel);
-
-    let mut next_page_token: Option<String> = None;
-
-    loop {
-        let req = LiveChatMessageListRequest {
-            part: vec!["snippet".to_string()],
-            live_chat_id: live_chat_id.clone(),
-            max_results: Some(20),
-            page_token: next_page_token.clone(),
-            profile_image_size: Some(0),
-            hl: Some("en".to_string()),
-        };
-
-        let mut request = Request::new(req);
-        let auth: MetadataValue<_> = format!("Bearer {}", token).parse()?;
-        request.metadata_mut().insert("authorization", auth);
-
-        let mut stream = client.stream_list(request).await?.into_inner();
-
-        while let Some(resp) = stream.message().await? {
-            println!("{resp:?}");
-
-            next_page_token = resp.next_page_token.clone();
-        }
-
-        if next_page_token.is_none() {
-            break;
+            Ok(())
         }
     }
-
-    Ok(())
 }
