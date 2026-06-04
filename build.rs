77c93e18c964541a1fdfa93205c64435b60c2ccf
efekrskl <8130264+efekrskl@users.noreply.github.com>
2026-06-04 22:01:56 +0200

    fix: protoc binary

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+    unsafe {
+        std::env::set_var("PROTOC", protoc);
+    }
     tonic_prost_build::compile_protos("proto/stream_list.proto")?;
     Ok(())
 }
